b7048900d89d44c59214c66aff31422e52cce66e
natch <natsume.1129k@gmail.com>
2026-05-27 11:27:29 +0900

    Fix production API URL

--- src/web/config.js
+++ src/web/config.js
@@ -1,5 +1,6 @@
-const config = {
-  apiUrl: 'http://localhost:5642'
+export const config = {
+  apiUrl:
+    location.hostname === 'localhost' || location.hostname === '127.0.0.1'
+      ? 'http://localhost:5642'
+      : '/api_na-kojima'
 };
-
-export default config;
